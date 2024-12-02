262aef4eee959d0de5f04ca55eb0e8df107c4a4c
kzioudas <kzioudass@gmail.com>
2024-12-02 03:38:16 +0200

    [parser only] added ThrowsExceptionDirective

--- src/main/java/codepilotunittest/directives/DirectiveFactory.java
+++ src/main/java/codepilotunittest/directives/DirectiveFactory.java
@@ -6,35 +6,26 @@ public class DirectiveFactory {
     /**
      * Factory method to create a directive based on a string.
      *
-     * @param paramName
-     * @param inputValue
-     * @param responceExpected
-     * @param expected
-     * @return a Directive object representing the directive
+     * @param paramName        Name of the parameter.
+     * @param inputValue       Input value provided for the directive.
+     * @param responseExpected The expected response from the method.
+     * @param expected         The expected outcome of the test case.
+     * @return a Directive object representing the directive.
      */
-    public static Directive createDirective(String paramName, String inputValue, String responceExpected,String expected) {
+    public static Directive createDirective(String paramName, String inputValue, String responseExpected,String expected) {
         if (expected.equalsIgnoreCase("True")||expected.equalsIgnoreCase("False")) {
             if (inputValue.equalsIgnoreCase("NULL")) {
-                return new NullDirective(paramName, inputValue, responceExpected, expected);
+                return new NullDirective(paramName, inputValue, responseExpected, expected);
             } else if (inputValue.matches("range\\((\\d+)\\-(\\d+)\\)")) {
                 String[] rangeParts = inputValue.substring(6, inputValue.length() - 1).split("-");
                 int min = Integer.parseInt(rangeParts[0].trim());
                 int max = Integer.parseInt(rangeParts[1].trim());
-                return new RangeDirective(paramName, min, max, responceExpected, expected);
+                return new RangeDirective(paramName, min, max, responseExpected, expected);
             } else {
-                return new SimpleValueDirective(paramName, inputValue, responceExpected, expected);
+                return new SimpleValueDirective(paramName, inputValue, responseExpected, expected);
             }
-        } else if(expected.toLowerCase().contains("exception")) {
-            if (inputValue.equalsIgnoreCase("NULL")) {
-                return new NullDirective(paramName, inputValue, responceExpected, expected);
-            } else if (inputValue.matches("range\\((\\d+)\\-(\\d+)\\)")) {
-                String[] rangeParts = inputValue.substring(6, inputValue.length() - 1).split("-");
-                int min = Integer.parseInt(rangeParts[0].trim());
-                int max = Integer.parseInt(rangeParts[1].trim());
-                return new RangeDirective(paramName, min, max, responceExpected, expected);
-            } else {
-                return new SimpleValueDirective(paramName, inputValue, responceExpected, expected);
-            }
+        } else if (expected.toLowerCase().contains("exception")) {
+            return new ThrowsExceptionDirective(paramName, responseExpected, expected);
         }
         return null;
     }
